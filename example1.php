f1c932d05329360e7b99985e563114c25be1b6b2
thomas <thomas@t-sauer.de>
2015-11-16 09:57:12 +0100

    init

---
+++ example1.php
@@ -0,0 +1,16 @@
+<?php
+header ("Content-type: text/html");
+?>
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8" />
+    <title>Hello World</title>
+</head>
+<body>
+<p> Hello
+    <?php
+    echo "World</p>";
+    ?>
+</body>
+</html>
